a6754573f2b171d019af2b879e551f5e18eb446d
leonxu0207 <741930067@qq.com>
2024-01-10 14:18:39 +0800

    feat: update子应用

---
+++ next-app/next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+const basePath = '/' // 默认为 '/'
+// 静态资源路径前缀
+// const assetPrefix = process.env.NODE_ENV === 'production' ? `线上域名${basePath}` : `http://localhost:${process.env.PORT || 3000}${basePath}`
+
+module.exports = {
+  // basePath,
+  // assetPrefix,
+  // 添加 assetPrefix 地址到 publicRuntimeConfig
+  // publicRuntimeConfig: {
+  //   assetPrefix,
+  // },
+}
